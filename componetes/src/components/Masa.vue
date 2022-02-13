6a73d192b3481a6f6d15f9cdc75bece3d305e842
ManfredyQuintanilla <97974376+ManfredyQuintanilla@users.noreply.github.com>
2022-02-13 12:21:24 -0600

    Conversosr de Masas 2.1

--- componetes/src/components/Masa.vue
+++ componetes/src/components/Masa.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+<div class="Body"> 
         <h1>Conversor de masa</h1>
-
+    <div class="Main">
         <div class="form-group">
 				<label for="oamount">
-					Cantidad a Convertir:
+					Ingrese su conversión:
 				</label>
 				<input type="text" v-model="entrada_numero"
 					class="form-control searchBox"
@@ -32,9 +32,15 @@
             <option value="T">Tonelada</option>
         </select>
         <br>
-        <button v-on:click="calcular">calcular</button>
-        <h2>Resultado:{{salida}}</h2>
+        <button v-on:click="calcular" class="btn btn-calcular">calcular</button>
     </div>
+    <div id="Resultado" class="text-center">
+        <h2>Resultado:{{salida}}</h2>
+        <span class="finalValue"
+					style="color:green;">
+		</span>
+    </div>
+</div>
 </template>
 <script>
 
@@ -43,7 +49,7 @@ export default ({
     data(){
         return{
             entrada_numero:"",
-            conversiones_gramos:{ "g":1,"kg":0.001,"onz":0.0237,"lb":0.002205,"T":0.000001102},    
+            conversiones_gramos:{ "g":1,"kg":0.001,"onz":0.03528,"lb":0.002205,"T":0.000001102},    
             salida: "",
             toRate: "g",
             fromRate:"kg"
@@ -59,3 +65,23 @@ export default ({
 })
 
 </script>
+<style>
+.Body {
+	background-color: aliceblue;
+	background-position: center;
+	background-size: cover;
+	background-attachment: fixed;
+	background-repeat: no-repeat;
+}
+.Main {
+	width: 50vw;
+	margin: auto;
+	padding: 30px;
+	border-radius: 5px;
+	background-color: rgba(255, 0, 0, 0.472);
+	color: rgb(0, 0, 0);
+}
+#Resultado h2 {
+	font-size: 45px;
+}
+</style>
